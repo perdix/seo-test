bf44ac9a72d2f6d6aaea93ec188bb729a9c7471c
Paul Opitz <paul.opitz@outlook.com>
2026-03-04 12:27:51 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import background from '$lib/assets/background.jpg'
+</script>
+<header class="h-[500px] bg-center bg-cover flex items-center justify-center" 
+   style="background-image:url({background})">
+    <h1 class="text-5xl">Electronic Devices</h1>
+</header>
+
+<main>
+
+</main>
+
+<footer>
+
+</footer>
