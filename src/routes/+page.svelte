78f1ec919fb27034b89450ba1a73130bea1ba452
Paul Opitz <paul.opitz@outlook.com>
2026-03-04 12:40:49 +0100

    title tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script>
     import background from '$lib/assets/background.jpg'
 </script>
+
+<svelte:head>
+    <title>Paul's Webshop - Electronics for all!</title>
+    <meta name="description" content="We sell used electronics for a super cheap price!">
+</svelte:head>
+
+
 <header class="h-[500px] bg-center bg-cover flex items-center justify-center" 
    style="background-image:url({background})">
     <h1 class="text-5xl">Electronic Devices</h1>
